c63ae6501fbeb5abbb6fa7c02130dbede9352489
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-01 20:27:27 +0300

    style(DialogBoxApp): Set structure to the dialog box components

--- src/Projects/13-dialog-box-app/DialogBoxApp.jsx
+++ src/Projects/13-dialog-box-app/DialogBoxApp.jsx
@@ -1,7 +1,26 @@
+import { Button } from '../../components'
+
 const DialogBoxApp = ({ width = 360 }) => {
   return (
     <div className='card bg-light m-auto mt-4' style={{ width: width }}>
-      DialogBoxApp
+      <div className='card-body'>
+        <div
+          className='d-grid aic jcc'
+          style={{ gridTemplateColumns: '2fr 1fr', alignSelf: 'center' }}
+        >
+          <section>
+            <h2 className='card-title'>Title</h2>
+            <h3 className='card-text'>Text</h3>
+          </section>
+          <div>Icon</div>
+        </div>
+      </div>
+
+      <div className='d-flex mb-2 px-1 text-end'>
+        <Button text='Cancel' classes={'btn-light'} />
+        <Button text='Subscribe' classes={'btn-danger'} />
+        <Button text='Unsubscribe' classes={'btn-danger btn-block'} />
+      </div>
     </div>
   )
 }
