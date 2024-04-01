47faac27a0f0d5d449559b1d150f4b550ff609b9
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-01 17:26:23 +0300

    feat(ProgressBarComp): Add codebase for new component

--- src/Projects/12-progress-tracker-app/ProgressTrackerApp.jsx
+++ src/Projects/12-progress-tracker-app/ProgressTrackerApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { ProgressBar, Title } from '../../components'
 const ProgressTrackerApp = () => {
   return (
     <div className='container'>
       <Title title={'Progress Tracker App'} classes={'title text-center'} />
+      <ProgressBar />
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Alert from './Alert'
 import LockSlider from './LockSlider'
 import Slider from './Slider'
 import Popup from './Popup'
+import ProgressBar from './ProgressBar'
 
-export { Popup, Title, Button, Alert, LockSlider, Slider }
+export { ProgressBar, Popup, Title, Button, Alert, LockSlider, Slider }
