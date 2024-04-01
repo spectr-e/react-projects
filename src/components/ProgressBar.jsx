37f5f84518cf4d5d4ae7eedf7935eb4b6bba5712
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-01 18:43:03 +0300

    style(ProgressBarComp): Create styling fo the progress bars

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+export const ProgressBar = ({ bgColor, completed }) => {
+  const containerStyles = {
+    height: 25,
+    backgroundColor: '#e0e0de',
+    borderRadius: 50,
+    margin: 50,
+    width: '100%',
+  }
+  const fillerStyles = {
+    backgroundColor: bgColor,
+    height: '100%',
+    width: `${completed}`,
+    borderRadius: 'inherit',
+    textAlign: 'right',
+    transition: 'width 1s ease-in-out',
+  }
+  const labelStyles = {
+    padding: 15,
+    margin: 10,
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 
-const ProgressBar = () => {
-  return <div>ProgressBar</div>
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed} %`}</span>
+      </div>
+    </div>
+  )
 }
-
-export default ProgressBar
--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,6 @@ import Alert from './Alert'
 import LockSlider from './LockSlider'
 import Slider from './Slider'
 import Popup from './Popup'
-import ProgressBar from './ProgressBar'
+import { ProgressBar } from './ProgressBar'
 
 export { ProgressBar, Popup, Title, Button, Alert, LockSlider, Slider }
