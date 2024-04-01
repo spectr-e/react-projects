9699bdc138bc43fb03ef7e2e7098e38534b80275
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-01 17:30:40 +0300

    style(App.css): Add support for gap spacing

--- src/Projects/12-progress-tracker-app/ProgressTrackerApp.jsx
+++ src/Projects/12-progress-tracker-app/ProgressTrackerApp.jsx
@@ -4,6 +4,30 @@ const ProgressTrackerApp = () => {
   return (
     <div className='container'>
       <Title title={'Progress Tracker App'} classes={'title text-center'} />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 8,
+          // justifyContent: 'center',
+        }}
+      >
+        <div className=''>
+          <div style={{ display: 'flex', gap: 8 }}>
+            <label htmlFor='ui'>UI</label>
+            <input type='text' name='UI' />
+          </div>
+          <div style={{ display: 'flex', gap: 8 }}>
+            <label htmlFor='UX'>UX</label>
+            <input type='text' name='UX' />
+          </div>
+          <div style={{ display: 'flex', gap: 8 }}>
+            <label htmlFor='data'>Data</label>
+            <input type='text' name='data' />
+          </div>
+        </div>
+      </div>
       <ProgressBar />
     </div>
   )
