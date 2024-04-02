763e289ed93474b48b2708c98c70727451250825
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-02 08:05:03 +0300

    feat(FilterContactApp): Create a search bar component

--- src/Projects/14-filter-contact-app/FilterContactApp.jsx
+++ src/Projects/14-filter-contact-app/FilterContactApp.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useRef, useState } from 'react'
 import { Title } from '../../components'
 
 const FilterContactApp = () => {
+  const inputRef = useRef(null)
+  const [searchVal, setSearchVal] = useState('')
+
+  const handleSearch = (e) => {
+    setSearchVal(e.target.value)
+  }
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
   return (
-    <div className='container'>
-      <Title title={'Contacts'} classes={'title text-center'} />
+    <div className='container text-center'>
+      <Title title={'Contacts'} />
+      <input
+        type='text'
+        className='mb-2 text-center font-md'
+        placeholder='Search by name'
+        value={searchVal}
+        onChange={handleSearch}
+        ref={inputRef}
+        style={{
+          padding: '4px 8px',
+        }}
+      />
     </div>
   )
 }
