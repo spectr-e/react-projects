d76930b37df3661a544dd0545a4b833ae89ce79e
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-03 13:33:27 +0300

    fix(RegistrationForm): Add focus to first field upon initialization & prevent refresh on submit

--- src/Projects/15-registration-form-app/RegistrationForm.jsx
+++ src/Projects/15-registration-form-app/RegistrationForm.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useRef } from 'react'
 import { Button, Title } from '../../components'
 
 const RegistrationForm = () => {
+  const inputName = useRef(null)
+  const inputEmail = useRef(null)
+  const inputPassword = useRef(null)
+
+  useEffect(() => {
+    inputName.current.focus()
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className='m-auto'>
       <div className='container bg-light text-dark mt-4 p-2 shadow-sm border-5 w-50'>
         <Title title={'Register'} />
-        <form action='' className=''>
+        <form action='' className='' onSubmit={handleSubmit}>
           {/* Name Field */}
           <div className='form-group'>
-            <input type='text' placeholder='Enter your name' name='fullname' />
+            <input
+              type='text'
+              placeholder='Enter your name'
+              name='fullname'
+              ref={inputName}
+            />
             <label htmlFor='fullname' className='text-danger font-sm'>
               Please enter your name
             </label>
@@ -16,7 +34,12 @@ const RegistrationForm = () => {
 
           {/* Email Field */}
           <div className='form-group'>
-            <input type='email' placeholder='Enter your email' name='email' />
+            <input
+              type='email'
+              placeholder='Enter your email'
+              name='email'
+              ref={inputEmail}
+            />
             <label htmlFor='email' className='text-danger font-sm'>
               Please enter your email
             </label>
@@ -28,6 +51,7 @@ const RegistrationForm = () => {
               type='password'
               placeholder='Enter your password'
               name='password'
+              ref={inputPassword}
             />
             <label htmlFor='password' className='text-danger font-sm'>
               Please enter your password
