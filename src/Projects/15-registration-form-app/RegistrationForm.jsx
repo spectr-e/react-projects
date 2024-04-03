666b697256256fc1095489d4314e797bf9905f3b
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-03 13:29:02 +0300

    feat(RegistrationForm): Add submit button to the form

--- src/Projects/15-registration-form-app/RegistrationForm.jsx
+++ src/Projects/15-registration-form-app/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import { Title } from '../../components'
+import { Button, Title } from '../../components'
 
 const RegistrationForm = () => {
   return (
@@ -6,18 +6,23 @@ const RegistrationForm = () => {
       <div className='container bg-light text-dark mt-4 p-2 shadow-sm border-5 w-50'>
         <Title title={'Register'} />
         <form action='' className=''>
+          {/* Name Field */}
           <div className='form-group'>
             <input type='text' placeholder='Enter your name' name='fullname' />
             <label htmlFor='fullname' className='text-danger font-sm'>
               Please enter your name
             </label>
           </div>
+
+          {/* Email Field */}
           <div className='form-group'>
             <input type='email' placeholder='Enter your email' name='email' />
             <label htmlFor='email' className='text-danger font-sm'>
               Please enter your email
             </label>
           </div>
+
+          {/* Password Field */}
           <div className='form-group'>
             <input
               type='password'
@@ -28,6 +33,9 @@ const RegistrationForm = () => {
               Please enter your password
             </label>
           </div>
+
+          {/* Submit Button */}
+          <Button classes={'btn-primary btn-lg btn-block'} text='Register' />
         </form>
       </div>
     </div>
