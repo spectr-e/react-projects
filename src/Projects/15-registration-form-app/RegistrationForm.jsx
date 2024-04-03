476709920ff62b2f11687e5132b94b849de31d03
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-03 13:27:01 +0300

    feat(RegistrationForm): Add input fields with labels to the app

--- src/Projects/15-registration-form-app/RegistrationForm.jsx
+++ src/Projects/15-registration-form-app/RegistrationForm.jsx
@@ -1,5 +1,37 @@
+import { Title } from '../../components'
+
 const RegistrationForm = () => {
-  return <div>RegistrationForm</div>
+  return (
+    <div className='m-auto'>
+      <div className='container bg-light text-dark mt-4 p-2 shadow-sm border-5 w-50'>
+        <Title title={'Register'} />
+        <form action='' className=''>
+          <div className='form-group'>
+            <input type='text' placeholder='Enter your name' name='fullname' />
+            <label htmlFor='fullname' className='text-danger font-sm'>
+              Please enter your name
+            </label>
+          </div>
+          <div className='form-group'>
+            <input type='email' placeholder='Enter your email' name='email' />
+            <label htmlFor='email' className='text-danger font-sm'>
+              Please enter your email
+            </label>
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Enter your password'
+              name='password'
+            />
+            <label htmlFor='password' className='text-danger font-sm'>
+              Please enter your password
+            </label>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default RegistrationForm
