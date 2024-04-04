2fb2928beab63df978578dcaedd19979f4bd4aec
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-04 05:10:33 +0300

    fix(NotesApp): Add basic structure for the + button and notes comp

--- src/Projects/16-notes-app/NotesApp.jsx
+++ src/Projects/16-notes-app/NotesApp.jsx
@@ -1,5 +1,18 @@
+import { Button, Notes } from '../../components'
+
 const NotesApp = () => {
-  return <div>NotesApp</div>
+  return (
+    <div className='container'>
+      <Button classes={'btn-success'} text='+ New Note' />
+
+      <div
+        className='container container-sm d-flex gap-24'
+        style={{ flexWrap: 'wrap' }}
+      >
+        <Notes />
+      </div>
+    </div>
+  )
 }
 
 export default NotesApp
