35640e4840336dbb6a346ad3edad91d75ba3ead0
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-04 10:06:12 +0300

    fix(NavBar): Add structure for the links to be included

--- src/Projects/17-responsive-navigation-app/NavBar.jsx
+++ src/Projects/17-responsive-navigation-app/NavBar.jsx
@@ -1,5 +1,25 @@
 const NavBar = () => {
-  return <nav>NavBar</nav>
+  return (
+    <nav>
+      <div className='navbar-logo'>
+        <a href='#' className='navbar-brand'>
+          Kamauu
+        </a>
+
+        {/* Open & Close Icons */}
+      </div>
+
+      <div className='navbar-collapse'>
+        <ul className='navbar-items'>
+          <li className='nav-item'>
+            <a href='#' className='nav-link'>
+              Home
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
