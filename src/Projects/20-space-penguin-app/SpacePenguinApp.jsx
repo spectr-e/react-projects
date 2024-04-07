18c6ec91ce3184246504c1b96fd14ac3b1a7e688
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-07 19:14:57 +0300

    fix(SpacePenguinApp): Add the fly and land functions to the ship

--- src/Projects/20-space-penguin-app/SpacePenguinApp.jsx
+++ src/Projects/20-space-penguin-app/SpacePenguinApp.jsx
@@ -1,17 +1,29 @@
 import './Ship.css'
 import { Button, Moon, Planet, SpaceShip, Title } from '../../components'
 import { Earth, Ship, TheMoon } from '../../assets'
+import { useState } from 'react'
 
 const SpacePenguinApp = () => {
+  const [shipAct, setShipAct] = useState()
+  const [transformScale, setTransformScale] = useState(200)
+
+  const startShip = () => {
+    setShipAct('fly')
+    setTransformScale(50)
+  }
+  const landShip = () => {
+    setShipAct('land')
+    setTransformScale(200)
+  }
   return (
     <div className='space'>
-      <Button text='Launch' classes={'btn-danger'} />
+      <Button text='Launch' classes={'btn-danger'} handleClick={startShip} />
       <section className='env'>
         <SpaceShip ship={Ship} scale={200} />
         <Moon moon={TheMoon} />
         <Planet planet={Earth} />
       </section>
-      <Button text='Land' classes={'btn-primary'} />
+      <Button text='Land' classes={'btn-primary'} handleClick={landShip} />
     </div>
   )
 }
