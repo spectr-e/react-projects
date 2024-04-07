a439cb3c6b217389df06f15956231230c3c79931
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-07 11:28:19 +0300

    feat(CompoundInterestCalcApp): Add form for collecting investment data using card styling

--- src/Projects/19-compound-interest-calc-app/CompoundInterestCalcApp.jsx
+++ src/Projects/19-compound-interest-calc-app/CompoundInterestCalcApp.jsx
@@ -1,4 +1,4 @@
-import { Title } from '../../components'
+import { Form, Title } from '../../components'
 
 const CompoundInterestCalcApp = () => {
   return (
@@ -7,6 +7,40 @@ const CompoundInterestCalcApp = () => {
         title={'Compound Interest Calculator'}
         classes={'subtitle text-center'}
       />
+
+      <div
+        className='card card-primary bg-warning shadow-md text-dark m-auto mt-4'
+        style={{ maxWidth: 400 }}
+      >
+        <div className='card-body'>
+          <Form
+            labelText={'Principal Amount (KES)'}
+            inputType='number'
+            values=''
+            classes={'mb-1'}
+          />
+          <Form
+            labelText={'Monthly Contribution (KES)'}
+            inputType='number'
+            values=''
+            classes={'mb-1'}
+          />
+          <Form
+            labelText={'Interest Rate (%)'}
+            inputType='number'
+            values=''
+            classes={'mb-1'}
+          />
+          <Form
+            labelText={'Period (yrs)'}
+            inputType='number'
+            values=''
+            classes={'mb-1'}
+          />
+        </div>
+
+        <div className='card-footer text-right font-lg'>KES 0.00</div>
+      </div>
     </div>
   )
 }
