9d131b35b23fe44d7f435f7980b09dc7ec2dce0a
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-11 20:40:21 +0300

    fix(CardComp): Build the card header to completion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,12 @@
-const Card = ({ player }) => {
+import { Shield, Swords } from '../assets'
+
+const Card = ({ player, defenseVal, attackVal }) => {
+  const iconStyle = {
+    marginRight: 10,
+    width: 20,
+    background: 'transparent',
+  }
+
   return (
     <div
       className={`card text-center m-1 shadow-md ${
@@ -6,7 +14,19 @@ const Card = ({ player }) => {
       }`}
       style={{ maxWidth: 180 }}
     >
-      Card
+      <section
+        className='card-header d-flex'
+        style={{ justifyContent: 'space-between' }}
+      >
+        <div className='defense'>
+          <img src={Shield} alt='defense' style={iconStyle} />
+          <span>{!defenseVal ? '00' : defenseVal}</span>
+        </div>
+        <div className='attack'>
+          <img src={Swords} alt='attack' style={iconStyle} />
+          <span>{!attackVal ? '00' : attackVal}</span>
+        </div>
+      </section>
     </div>
   )
 }
