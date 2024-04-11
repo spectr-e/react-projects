c9bcb2c668c324c66bb74b92a6be1445368ea834
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-11 20:24:05 +0300

    style(CardGame): Add custom css for the card game app

--- src/Projects/22-card-game-app/CardGameApp.jsx
+++ src/Projects/22-card-game-app/CardGameApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Button, Title } from '../../components'
+import './CardGame.css'
 
 const CardGameApp = () => {
   const [start, setStart] = useState(false)
