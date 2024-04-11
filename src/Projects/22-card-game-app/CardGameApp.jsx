fdcad2cc5ccdba34c26afefe585b65d496cbcee7
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-11 20:22:30 +0300

    fix(CardGameApp): Add button and rules for the start page

--- src/Projects/22-card-game-app/CardGameApp.jsx
+++ src/Projects/22-card-game-app/CardGameApp.jsx
@@ -1,9 +1,36 @@
-import { Title } from '../../components'
+import { useState } from 'react'
+import { Button, Title } from '../../components'
 
 const CardGameApp = () => {
+  const [start, setStart] = useState(false)
+  document.body.style.background = '#170536'
+  document.body.style.color = '#bab6bf'
+
   return (
-    <div className='container text-center'>
-      <Title title={'Card Game'} />
+    <div className='container'>
+      {/* {!start ? } */}
+      <section className='text-center'>
+        <Title title={'Card Game'} />
+        <Button text='Start' classes={'btn-success btn-lg'} />
+        <Title classes={'subtitle'} title={'Rules:'} />
+        <ul className='font-md dsf flex-column'>
+          <li>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+            ipsam? Alias ducimus delectus consequuntur iste tempora consectetur
+            reiciendis cumque. Veniam.
+          </li>
+          <li>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+            ipsam? Alias ducimus delectus consequuntur iste tempora consectetur
+            reiciendis cumque. Veniam.
+          </li>
+          <li>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+            ipsam? Alias ducimus delectus consequuntur iste tempora consectetur
+            reiciendis cumque. Veniam.
+          </li>
+        </ul>
+      </section>
     </div>
   )
 }
