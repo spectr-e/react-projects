c165901cdce7cef951e46fa7dce52284252464cc
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-11 20:28:16 +0300

    fix(CardGameApp): Add second page where game will be after start is clicked

--- src/Projects/22-card-game-app/CardGameApp.jsx
+++ src/Projects/22-card-game-app/CardGameApp.jsx
@@ -4,34 +4,48 @@ import './CardGame.css'
 
 const CardGameApp = () => {
   const [start, setStart] = useState(false)
+
+  const startGame = () => {
+    setStart(true)
+  }
+
   document.body.style.background = '#170536'
   document.body.style.color = '#bab6bf'
 
   return (
-    <div className='container'>
-      {/* {!start ? } */}
-      <section className='text-center'>
-        <Title title={'Card Game'} />
-        <Button text='Start' classes={'btn-success btn-lg'} />
-        <Title classes={'subtitle'} title={'Rules:'} />
-        <ul className='font-md dsf flex-column'>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            ipsam? Alias ducimus delectus consequuntur iste tempora consectetur
-            reiciendis cumque. Veniam.
-          </li>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            ipsam? Alias ducimus delectus consequuntur iste tempora consectetur
-            reiciendis cumque. Veniam.
-          </li>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            ipsam? Alias ducimus delectus consequuntur iste tempora consectetur
-            reiciendis cumque. Veniam.
-          </li>
-        </ul>
-      </section>
+    <div className='container text-center'>
+      {!start ? (
+        <section>
+          <Title title={'Card Game'} />
+          <Button
+            text='Start'
+            classes={'btn-success btn-lg'}
+            handleClick={startGame}
+          />
+          <Title classes={'subtitle'} title={'Rules:'} />
+          <ul className='font-md dsf flex-column'>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+              ipsam? Alias ducimus delectus consequuntur iste tempora
+              consectetur reiciendis cumque. Veniam.
+            </li>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+              ipsam? Alias ducimus delectus consequuntur iste tempora
+              consectetur reiciendis cumque. Veniam.
+            </li>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+              ipsam? Alias ducimus delectus consequuntur iste tempora
+              consectetur reiciendis cumque. Veniam.
+            </li>
+          </ul>
+        </section>
+      ) : (
+        <>
+          <Title title={'0-1'} />
+        </>
+      )}
     </div>
   )
 }
