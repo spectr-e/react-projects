93d500527319ed059ee8316e1b05bdfcefeba675
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-11 20:16:29 +0300

    feat(Assets): Add svgs for project 22

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,5 +6,28 @@ import TheMoon from './moon.svg'
 import Ship from './ship.svg'
 import Rocket from './rocket.svg'
 import Penguin from './penguin.svg'
+import Horn from './horn.svg'
+import Horseman from './horseman.svg'
+import Shield from './shield.svg'
+import Archer from './archer.svg'
+import Spearman from './spearman.svg'
+import Swords from './swords.svg'
+import Warhorse from './warhorse.svg'
 
-export { Dog, LockImg, HomeImg, Earth, Ship, Rocket, TheMoon, Penguin }
+export {
+  Dog,
+  LockImg,
+  HomeImg,
+  Earth,
+  Ship,
+  Rocket,
+  TheMoon,
+  Penguin,
+  Horn,
+  Horseman,
+  Shield,
+  Archer,
+  Spearman,
+  Swords,
+  Warhorse,
+}
