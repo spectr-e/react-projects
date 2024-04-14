cf9800fdd01e074cfd7e4356e21c2ac4a41f433f
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-14 19:55:41 +0300

    style(AnimateLoginForm): Add a refactor styling to the form element

--- src/Projects/23-animated-login-form-app/AnimatedLoginForm.jsx
+++ src/Projects/23-animated-login-form-app/AnimatedLoginForm.jsx
@@ -1,5 +1,16 @@
 const AnimatedLoginForm = () => {
-  return <div>AnimatedLoginForm</div>
+  const formStyle = {
+    width: 360,
+    marginTop: '15vh',
+  }
+
+  return (
+    <div className='container'>
+      <form action='' style={formStyle} className='card m-1 m-auto'>
+        form
+      </form>
+    </div>
+  )
 }
 
 export default AnimatedLoginForm
