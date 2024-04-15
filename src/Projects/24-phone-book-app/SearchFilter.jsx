09600ed6939152cb3b576ee71cbb384c01d533cd
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-15 22:11:26 +0300

    style(SearchFilterComp): Format the component style

--- src/Projects/24-phone-book-app/SearchFilter.jsx
+++ src/Projects/24-phone-book-app/SearchFilter.jsx
@@ -1,9 +1,17 @@
+import { FaAddressBook, FaArrowsAltV } from 'react-icons/fa'
 import { Button } from '../../components'
 
 const SearchFilter = () => {
   return (
     <section className='text-center'>
-      <Button classes={'btn-dark'} />
+      <Button classes={'btn-dark'} text={''} icon={<FaAddressBook />} />
+      <input
+        type='text'
+        placeholder='Search by first name'
+        className='mb-2 mx-1'
+        style={{ padding: '4px 8px' }}
+      />
+      <Button classes={'btn-dark'} text={''} icon={<FaArrowsAltV />} />
     </section>
   )
 }
