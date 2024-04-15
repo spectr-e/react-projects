cdd402f1a0dbb76f3650427facd72a79e27eb5e8
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-15 21:10:20 +0300

    style(PhoneBookApp): Create a contacts container within the main container

--- src/Projects/24-phone-book-app/PhoneBookApp.jsx
+++ src/Projects/24-phone-book-app/PhoneBookApp.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import { Title } from '../../components'
 
 const PhoneBookApp = () => {
+  const [showContacts, setShowContacts] = useState(false)
+
   return (
     <div className='container text-center'>
       <Title title={'Phonebook App'} />
+      <main className='bg-dark text-light p-1'>
+        <div style={{ height: 600, overflow: 'auto' }}>
+          <h2 className='subtitle text-center'>Display Contacts</h2>
+        </div>
+      </main>
     </div>
   )
 }
