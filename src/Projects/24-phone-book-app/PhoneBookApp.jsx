3c1cc3c70a66e47b164bbb39f88e887fdf9c7df3
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-15 22:08:18 +0300

    feat(SearchFilterComp): Initialize the codebase for the search filter component

--- src/Projects/24-phone-book-app/PhoneBookApp.jsx
+++ src/Projects/24-phone-book-app/PhoneBookApp.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Title } from '../../components'
 import ContactList from './ContactList'
 import { contacts } from '../../data'
+import SearchFilter from './SearchFilter'
 
 const PhoneBookApp = () => {
   const [showContacts, setShowContacts] = useState(true)
@@ -11,6 +12,7 @@ const PhoneBookApp = () => {
     <div className='container'>
       <Title title={'Phonebook App'} classes={'title text-center'} />
       <main className='bg-dark text-light p-1'>
+        <SearchFilter />
         <div style={{ height: 600, overflow: 'auto' }}>
           <h2 className='subtitle text-center'>Display Contacts</h2>
           {showContacts && <ContactList contacts={db} />}
---
+++ src/Projects/24-phone-book-app/SearchFilter.jsx
@@ -0,0 +1,11 @@
+import { Button } from '../../components'
+
+const SearchFilter = () => {
+  return (
+    <section className='text-center'>
+      <Button classes={'btn-dark'} />
+    </section>
+  )
+}
+
+export default SearchFilter
