4df9685ff49089074b17db784a20d1aeb201d7eb
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-16 16:40:37 +0300

    fix(ExpenseCalculatorApp): Create layout for the expenses form component and the expenses totals

--- src/Projects/25-expenses-calculator-app/ExpenseCalculatorApp.jsx
+++ src/Projects/25-expenses-calculator-app/ExpenseCalculatorApp.jsx
@@ -12,7 +12,22 @@ const ExpenseCalculatorApp = () => {
           gap: 24,
           margin: 16,
         }}
-      ></section>
+      >
+        <aside>
+          {/* ExpensesForm Component */}
+          <section className='card mt-2 bg-primary text-light'>
+            <div className='card-body'>
+              {/* <BudgetStyle>
+                <h3>Budget: KES</h3>
+                <input type='number' value={''} />
+              </BudgetStyle> */}
+              <h3 className='mb-1'>Total Expenses: KES</h3>
+              {/* Calculate Savings */}
+              <h3>Savings: KES</h3>
+            </div>
+          </section>
+        </aside>
+      </section>
     </main>
   )
 }
