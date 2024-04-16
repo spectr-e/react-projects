3847b8d209e543cd660ad01a5677c98b707e2dc3
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-16 16:37:00 +0300

    fix(ExpenseCalculatorApp): Create grid structure to hold the expenses list, balances and expenses form

--- src/Projects/25-expenses-calculator-app/ExpenseCalculatorApp.jsx
+++ src/Projects/25-expenses-calculator-app/ExpenseCalculatorApp.jsx
@@ -1,5 +1,20 @@
+import { Title } from '../../components'
+
 const ExpenseCalculatorApp = () => {
-  return <div>ExpenseCalculatorApp</div>
+  return (
+    <main className='container'>
+      <Title title={'Expense Calculator'} />
+      {/* TODO: Alert Component */}
+      <section
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 1fr)',
+          gap: 24,
+          margin: 16,
+        }}
+      ></section>
+    </main>
+  )
 }
 
 export default ExpenseCalculatorApp
