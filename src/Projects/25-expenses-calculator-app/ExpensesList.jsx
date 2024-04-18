30260ae1767cc3012e690ed45720cc2d6404609c
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-18 17:17:18 +0300

    fix(ExpensesListComp): Add a clear all button

--- src/Projects/25-expenses-calculator-app/ExpensesList.jsx
+++ src/Projects/25-expenses-calculator-app/ExpensesList.jsx
@@ -1,5 +1,18 @@
-const ExpensesList = () => {
-  return <div>ExpensesList</div>
+import { Button } from '../../components'
+
+const ExpensesList = ({ clearItems, expenses = [] }) => {
+  return (
+    <>
+      <ul className='list'>{/* ExpenseItem */}</ul>
+      {expenses.length > 0 && (
+        <Button
+          text={`Clear all`}
+          classes={'btn-danger'}
+          handleClick={clearItems}
+        />
+      )}
+    </>
+  )
 }
 
 export default ExpensesList
