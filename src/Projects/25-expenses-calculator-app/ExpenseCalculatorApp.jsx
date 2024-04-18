2d3feb51d556f550a383d77aa2b82ae797c520d6
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-18 17:00:20 +0300

    fix(ExpenseCalcApp): Change how currency is displayed in totals

--- src/Projects/25-expenses-calculator-app/ExpenseCalculatorApp.jsx
+++ src/Projects/25-expenses-calculator-app/ExpenseCalculatorApp.jsx
@@ -18,15 +18,16 @@ const ExpenseCalculatorApp = () => {
           {/* ExpensesForm Component */}
           <ExpensesForm />
           <section className='card mt-2 bg-primary text-light'>
+            {/* Display Total */}
             <div className='card-body'>
               {/* <BudgetStyle>
                 <h3>Budget: KES</h3>
                 <input type='number' value={''} />
               </BudgetStyle> */}
 
-              <h3 className='mb-1'>Total Expenses: KES</h3>
+              <h3 className='mb-1'>Total Expenses (KES): </h3>
               {/* Calculate Savings */}
-              <h3>Savings: KES</h3>
+              <h3>Savings (KES): </h3>
             </div>
           </section>
         </aside>
