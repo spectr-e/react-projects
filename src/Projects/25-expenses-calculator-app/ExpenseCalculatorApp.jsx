8c09b3e4b2c011a9e8c486c9827801f41f4a6d59
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-18 18:13:22 +0300

    fix(ExpenseCalcApp): Add state variables for data handling

--- src/Projects/25-expenses-calculator-app/ExpenseCalculatorApp.jsx
+++ src/Projects/25-expenses-calculator-app/ExpenseCalculatorApp.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useRef, useState } from 'react'
 import { Title } from '../../components'
 import ExpensesForm from './ExpensesForm'
 import ExpensesList from './ExpensesList'
 import { BudgetStyle } from './styles/BudgetStyle'
 
 const ExpenseCalculatorApp = () => {
+  const [expenses, setExpenses] = useState('')
+  const [date, setDate] = useState('')
+  const [amount, setAmount] = useState('')
+  const [charge, setCharge] = useState('')
+  const [budget, setBudget] = useState('')
+
+  const inputBudget = useRef(null)
+  useEffect(() => {
+    inputBudget.current.focus()
+  }, [])
+
   return (
     <main className='container'>
       <Title title={'Expense Calculator'} classes={'text-center title'} />
@@ -19,14 +31,19 @@ const ExpenseCalculatorApp = () => {
       >
         <aside>
           {/* ExpensesForm Component */}
-          <ExpensesForm />
+          <ExpensesForm date={date} charge={charge} amount={amount} />
 
-          <section className='card mt-2 bg-primary text-light'>
+          <section className='card text-right mt-2 bg-primary text-light'>
             {/* Display Total */}
             <div className='card-body'>
               <BudgetStyle>
                 <h3>Budget (KES): </h3>
-                <input type='number' />
+                <input
+                  ref={inputBudget}
+                  type='number'
+                  value={budget}
+                  onChange={(e) => setBudget(e.target.value)}
+                />
               </BudgetStyle>
               <h3 className='mb-1'>Total Expenses (KES): </h3>
 
@@ -38,7 +55,7 @@ const ExpenseCalculatorApp = () => {
 
         <section>
           {/* ExpensesList Component */}
-          <ExpensesList />
+          <ExpensesList expenses={expenses} />
         </section>
       </section>
     </main>
