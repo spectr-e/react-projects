a02fbf0cee6012f5c1bc16284edab8a29b3d3df6
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-18 17:28:37 +0300

    fix(ExpenseItemComp): Add the structure to handle expense item details

--- src/Projects/25-expenses-calculator-app/ExpenseItem.jsx
+++ src/Projects/25-expenses-calculator-app/ExpenseItem.jsx
@@ -1,5 +1,28 @@
-const ExpenseItem = () => {
-  return <div>ExpenseItem</div>
+import { MdDelete, MdEdit } from 'react-icons/md'
+import { Button } from '../../components'
+
+const ExpenseItem = ({ charge, date, amount }) => {
+  return (
+    <div className='expense-details'>
+      <div className='d-flex flex-column' style={{ alignItems: 'flex-start' }}>
+        <h4 className='text-primary'>{charge}</h4>
+        <small className='text-danger'>{date}</small>
+      </div>
+      <div className='text-success'>${amount}</div>
+      <div className='d-flex flex-column'>
+        <Button
+          classes={'btn-sm'}
+          handleClick={() => handleEdit(id)}
+          text={<MdEdit />}
+        />
+        <Button
+          classes={'btn-sm btn-danger'}
+          handleClick={() => handleDelete(id)}
+          text={<MdDelete />}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default ExpenseItem
