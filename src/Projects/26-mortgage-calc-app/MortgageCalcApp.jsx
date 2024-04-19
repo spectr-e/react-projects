2d5b9da5e40fce21c5d050d10246b8d7db7c50a1
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-19 22:00:42 +0300

    fix(MortgageCalcApp): Add onChange event listener to control the input fields

--- src/Projects/26-mortgage-calc-app/MortgageCalcApp.jsx
+++ src/Projects/26-mortgage-calc-app/MortgageCalcApp.jsx
@@ -16,6 +16,29 @@ const MortgageCalcApp = () => {
     loanDuration: '',
   })
 
+  // Event handlers
+  // On Change
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case 'homeVal':
+        setFormData({ ...formData, homeVal: e.target.value })
+        break
+      case 'downPayment':
+        setFormData({ ...formData, downPayment: e.target.value })
+        break
+      case 'loanAmount':
+        setFormData({ ...formData, loanAmount: e.target.value })
+        break
+      case 'interestRate':
+        setFormData({ ...formData, interestRate: e.target.value })
+        break
+      case 'loanDuration':
+        setFormData({ ...formData, loanDuration: e.target.value })
+        break
+      default:
+        break
+    }
+  }
   return (
     <div className='container card mt-4' style={{ width: 500 }}>
       <Title title={'Mortgage Calculator'} classes={'title text-center'} />
@@ -29,35 +52,45 @@ const MortgageCalcApp = () => {
           }}
         >
           <Form
+            inputName={'homeVal'}
             inputType='number'
             labelText={'Home Value'}
             placeholderText={'Enter the value of the home'}
             refer={homeInputRef}
             values={formData.homeVal}
+            onChange={handleChange}
           />
           <Form
+            inputName={'downPayment'}
             inputType='number'
             labelText={'Down Payment'}
             placeholderText={'Enter your deposit amount'}
             values={formData.downPayment}
+            onChange={handleChange}
           />
           <Form
+            inputName={'loanAmt'}
             inputType='number'
             labelText={'Loan Amount'}
             placeholderText={'Enter the loan amount'}
             values={formData.loanAmt}
+            onChange={handleChange}
           />
           <Form
+            inputName={'interestRate'}
             inputType='number'
             labelText={'Interest Rate (%)'}
             placeholderText={'Enter the interest rate'}
             values={formData.interestRate}
+            onChange={handleChange}
           />
           <Form
+            inputName={'loanDuration'}
             inputType='number'
             labelText={'Loan Duration (Yrs)'}
             placeholderText={'Enter the loan duration'}
             values={formData.loanDuration}
+            onChange={handleChange}
           />
         </div>
         <Button
