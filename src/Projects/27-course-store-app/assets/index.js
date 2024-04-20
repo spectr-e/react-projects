a5543bb09dabeb1bd3d63dece23ae4f31fd2753d
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-20 16:46:42 +0300

    fix(CourseDB): Add all the courses

--- src/Projects/27-course-store-app/assets/index.js
+++ src/Projects/27-course-store-app/assets/index.js
@@ -3,6 +3,6 @@ import react from './react.jpg'
 import github from './github.png'
 import graphql from './graphql.jpg'
 import javascript from './javascript.jpg'
-import saas from './sass.jpg'
+import sass from './sass.jpg'
 
-export { bootstrap, react, graphql, github, saas, javascript }
+export { bootstrap, react, graphql, github, sass, javascript }
--- src/Projects/27-course-store-app/db/CourseDB.js
+++ src/Projects/27-course-store-app/db/CourseDB.js
@@ -1,10 +1,40 @@
-import { react } from '../assets'
+import { bootstrap, sass, github, graphql, javascript, react } from '../assets'
 
 export const CourseDB = [
   {
     id: 1,
     title: 'The Road to React',
-    price: 2999.99,
+    price: 19.99,
     img: react,
+  },
+  {
+    id: 2,
+    title: 'The Rout to GraphQL',
+    price: 29.99,
+    img: graphql,
+  },
+  {
+    id: '3',
+    title: 'The Road to JavaScript',
+    price: 14.99,
+    img: javascript,
+  },
+  {
+    id: '4',
+    title: 'Advance CSS and Sass',
+    price: 12.99,
+    img: sass,
+  },
+  {
+    id: '5',
+    title: 'Bootstrap5',
+    price: 10.99,
+    img: bootstrap,
+  },
+  {
+    id: '6',
+    title: 'Git and GitHub',
+    price: 10.99,
+    img: github,
   },
 ]
