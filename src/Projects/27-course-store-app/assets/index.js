f6aaa998bdfa0940072778404be54913a107b13e
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-20 16:25:14 +0300

    fix(Assets): Bring in image assets into project

---
+++ src/Projects/27-course-store-app/assets/index.js
@@ -0,0 +1,8 @@
+import bootstrap from './bootstrap.jpg'
+import react from './react.jpg'
+import github from './github.jpg'
+import graphql from './graphql.jpg'
+import javascript from './javascript.jpg'
+import saas from './saas.jpg'
+
+export { bootstrap, react, graphql, github, saas, javascript }
