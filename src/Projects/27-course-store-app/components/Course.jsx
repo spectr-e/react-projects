68b6c9df07330f24a50f5fed88ec6d06fd1ff9f1
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-20 16:12:58 +0300

    feat(CourseComp): Initialize  a seperate component to hold the design of the course card

---
+++ src/Projects/27-course-store-app/components/Course.jsx
@@ -0,0 +1,24 @@
+import { Button } from '../../../components'
+
+const Course = ({ title = 'Title' }) => {
+  return (
+    <li className={`card mb-2`} style={{ width: 240 }}>
+      <div className='card-header'>{title}</div>
+      <img src='' alt='course' style={{ height: '100%' }} />
+      <p className='card-body'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam vero
+        voluptas non ad magnam, atque, a maiores esse sit repellat hic quisquam
+        officia excepturi maxime dolor illum? Maiores, reprehenderit pariatur!
+      </p>
+      <div
+        className='card-footer'
+        style={{ display: 'flex', justifyContent: 'space-between' }}
+      >
+        <h4>Price</h4>
+        <Button classes={'btn-success'} text='Buy' />
+      </div>
+    </li>
+  )
+}
+
+export default Course
--- src/Projects/27-course-store-app/components/Courses.jsx
+++ src/Projects/27-course-store-app/components/Courses.jsx
@@ -1,12 +1,9 @@
+import Course from './Course'
+
 const Courses = () => {
   return (
     <ul className='d-flex mt-2' style={{ flexWrap: 'wrap', gap: 28 }}>
-      <li>Course 1</li>
-      <li>Course 2</li>
-      <li>Course 3</li>
-      <li>Course 4</li>
-      <li>Course 5</li>
-      <li>Course 6</li>
+      <Course />
     </ul>
   )
 }
