1c552b1cc6db3fbb5aa7a684c20ef81ab0bd5286
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-20 16:02:36 +0300

    fix(CourseStoreApp): Add a general design structure for the project

--- src/Projects/27-course-store-app/CourseStoreApp.jsx
+++ src/Projects/27-course-store-app/CourseStoreApp.jsx
@@ -1,10 +1,30 @@
-import { Title } from '../../components'
+import { Button, Title } from '../../components'
+
+document.body.style.backgroundColor = '#282c34'
+document.body.style.color = '#fff'
 
 const CourseStoreApp = () => {
   return (
-    <div className='container'>
+    <div className='container p-1'>
       {/* TODO: Add a Change Currency component here */}
+      <h4 className='font-medium mb-1'>Change Currency : </h4>
+      <div className='dsf'>
+        <Button classes={'btn-light btn-sm'} />
+        <Button classes={'btn-light btn-sm'} />
+        <Button classes={'btn-light btn-sm'} />
+      </div>
+
       <Title title={'Course Store'} classes={'title text-center'} />
+      <Title
+        title={'Become a Web Developer'}
+        classes={'subtitle text-center'}
+      />
+      <p className='mx-2 text-center'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde at
+        molestias repellat totam ipsam, dicta atque possimus eum eligendi dolore
+        beatae mollitia voluptatum quibusdam non nemo inventore voluptas odit
+        quia.
+      </p>
       {/* TODO: Add course cards here */}
     </div>
   )
