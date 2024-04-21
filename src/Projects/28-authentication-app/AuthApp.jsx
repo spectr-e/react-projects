7b5c9be50dfbb83d6b02bc5bcdeb91dc78ce6de7
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-21 14:48:21 +0300

    fix(AuthApp): Add context provider to app!

--- src/Projects/28-authentication-app/AuthApp.jsx
+++ src/Projects/28-authentication-app/AuthApp.jsx
@@ -5,12 +5,14 @@ import Auth from './components/Auth'
 import { AuthContext } from './context/AuthContext'
 
 const AuthApp = () => {
+  // ui controls
   const [ui, setUi] = useState({
     wrapper: true,
     title: 'Authenticate',
   })
-
   const [animateDeny, setAnimateDeny] = useState(false)
+
+  // focus on initialization
   const inputUser = useRef(null)
   useEffect(() => {
     inputUser.current.focus()
@@ -43,29 +45,31 @@ const AuthApp = () => {
   return (
     <div className='container' style={{ width: 240 }}>
       <Title title={ui.title} classes={'subtitle text-center'} />
-
-      {/* Form */}
-      {ui.wrapper && (
-        <Wrapper className={animateDeny && 'active'}>
-          <Form
-            labelText={'Username'}
-            inputName={'username'}
-            placeholderText={'Enter your username'}
-            values={formData.username}
-            onChange={handleChange}
-            refer={inputUser}
-          />
-          <Form
-            labelText={'Password'}
-            inputName={'password'}
-            inputType='password'
-            placeholderText={'Enter your password'}
-            values={formData.password}
-            onChange={handleChange}
-          />
-        </Wrapper>
-      )}
-      <Auth />
+      {/* context comes here */}
+      <AuthContext.Provider value={{ status: authStatus, login, logout }}>
+        {/* Form */}
+        {ui.wrapper && (
+          <Wrapper className={animateDeny && 'active'}>
+            <Form
+              labelText={'Username'}
+              inputName={'username'}
+              placeholderText={'Enter your username'}
+              values={formData.username}
+              onChange={handleChange}
+              refer={inputUser}
+            />
+            <Form
+              labelText={'Password'}
+              inputName={'password'}
+              inputType='password'
+              placeholderText={'Enter your password'}
+              values={formData.password}
+              onChange={handleChange}
+            />
+          </Wrapper>
+        )}
+        <Auth />
+      </AuthContext.Provider>
     </div>
   )
 }
