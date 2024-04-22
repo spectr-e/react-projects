4ff39791195aeb6984c9140f7cf1b453498885d9
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-22 10:36:15 +0300

    fix(SpeechSynthesizerApp): Add textarea controls with an onChange handler

--- src/Projects/29-speech-synthesizer-app/SpeechSynthesizerApp.jsx
+++ src/Projects/29-speech-synthesizer-app/SpeechSynthesizerApp.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import { Title } from '../../components'
 import SpeechNarrator from './components/SpeechNarrator'
 
 const SpeechSynthesizerApp = () => {
+  // textarea controls
+  const [text, setText] = useState('')
+  // textarea handlers
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
   return (
     <div className='container'>
       <Title title={'Speech Synthesizer'} classes={'title text-center'} />
       <SpeechNarrator />
+      <textarea
+        value={text}
+        onChange={handleChange}
+        id=''
+        cols='30'
+        rows='10'
+      ></textarea>
     </div>
   )
 }
