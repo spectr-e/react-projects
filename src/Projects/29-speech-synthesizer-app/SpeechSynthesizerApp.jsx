fea8cc7fe87cd796fb25bd5f68d9fe83fc408152
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-22 10:31:52 +0300

    feat(SpeechNarratorComp): Generate starter code for first component

--- src/Projects/29-speech-synthesizer-app/SpeechSynthesizerApp.jsx
+++ src/Projects/29-speech-synthesizer-app/SpeechSynthesizerApp.jsx
@@ -1,9 +1,11 @@
 import { Title } from '../../components'
+import SpeechNarrator from './components/SpeechNarrator'
 
 const SpeechSynthesizerApp = () => {
   return (
     <div className='container'>
       <Title title={'Speech Synthesizer'} classes={'title text-center'} />
+      <SpeechNarrator />
     </div>
   )
 }
---
+++ src/Projects/29-speech-synthesizer-app/components/SpeechNarrator.jsx
@@ -0,0 +1,5 @@
+const SpeechNarrator = () => {
+  return <div>SpeechNarrator</div>
+}
+
+export default SpeechNarrator
