376047a183a9d6eac7e8626fb5e52c4dc19b6ec4
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-22 10:41:29 +0300

    fix(SpeechNarratorComp): Create the robots and style them

--- src/Projects/29-speech-synthesizer-app/components/SpeechNarrator.jsx
+++ src/Projects/29-speech-synthesizer-app/components/SpeechNarrator.jsx
@@ -1,5 +1,17 @@
+import { AiFillRobot, AiOutlineRobot } from 'react-icons/ai'
+
 const SpeechNarrator = ({ text }) => {
-  return <div>SpeechNarrator</div>
+  const robotStyle = {
+    fontSize: 100,
+    padding: 0,
+    cursor: 'pointer',
+  }
+  return (
+    <div className='d-flex flex-column my-3 container' style={{ gap: 30 }}>
+      <AiFillRobot style={robotStyle} className='text-primary' />
+      <AiOutlineRobot style={robotStyle} />
+    </div>
+  )
 }
 
 export default SpeechNarrator
