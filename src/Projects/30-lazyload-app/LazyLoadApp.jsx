1b7f1a52b024fd002ce00168fc942d90c6a60950
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-23 11:49:08 +0300

    fix(LazyLoadApp): Fetch data from jsonplaceholder.com and add it to state

--- src/Projects/30-lazyload-app/LazyLoadApp.jsx
+++ src/Projects/30-lazyload-app/LazyLoadApp.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Title } from '../../components'
 
 const LazyLoadApp = () => {
   const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos/')
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+  }, [])
+
   return (
     <div className='container'>
       <Title
@@ -12,9 +19,8 @@ const LazyLoadApp = () => {
       <h1 className='text-center'>Post Images</h1>
       <div className='container d-flex flex-column' style={{ gap: 30 }}>
         {posts.map((post, index) => {
-          {
-            /* TODO: Create Lazy Load Post Component */
-          }
+          /* TODO: Create Lazy Load Post Component */
+          // <ImagePost key={index} title={post.title} img={post.url}/>
         })}
       </div>
     </div>
