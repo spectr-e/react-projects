c6f713306fe559612fad0164b32ad22ec4c1a220
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-23 11:44:24 +0300

    fix(LazyLoadApp): Create the structure for handling image posts

--- src/Projects/30-lazyload-app/LazyLoadApp.jsx
+++ src/Projects/30-lazyload-app/LazyLoadApp.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import { Title } from '../../components'
 
 const LazyLoadApp = () => {
+  const [posts, setPosts] = useState([])
   return (
     <div className='container'>
       <Title
         title={'Image Grid Scroll with Lazy Load'}
         classes={'text-center subtitle'}
       />
+      <h1 className='text-center'>Post Images</h1>
+      <div className='container d-flex flex-column' style={{ gap: 30 }}>
+        {posts.map((post, index) => {
+          {
+            /* TODO: Create Lazy Load Post Component */
+          }
+        })}
+      </div>
     </div>
   )
 }
